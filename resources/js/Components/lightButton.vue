f32b277c870c252d214dbaa9b68cd2a62c15afb3
ukki <iu.cashirskaia@yandex.ru>
2024-11-12 23:18:25 +0300

    надеюсь это все

---
+++ resources/js/Components/lightButton.vue
@@ -0,0 +1,8 @@
+<template>
+        <a class="relative" href="#">
+        <span class="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+        <span class="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">elevated button</span>
+                <!-- TODO добавить сюды переменную чтоб слету определять содержимое кнопки -->
+         <!-- TODO возможно объединить светлую и темные кнопки и выбирать их отображение по условию -->
+    </a>
+</template>
--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -1,9 +1,13 @@
 <script setup>
 import sliders from '@/Components/sliders.vue'
 import swi from '@/Components/switch.vue'
+import lightButton from '@/Components/lightButton.vue';
 </script>
 <template>
-    dsfasdglkdjsla
+    <div class="flex flex-wrap justify-center gap-6">
+<lightButton></lightButton>
+   
+</div>
     <swi></swi>
     <sliders></sliders>
 </template>
