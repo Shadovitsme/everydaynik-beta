3bfef1fcfbe6a1489aab4685a9bcbb506618b3b8
ukki <iu.cashirskaia@yandex.ru>
2024-11-12 11:40:04 +0300

    рабочий вью и новый компонент

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/test', function () {
+    return Inertia::render('test');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
