cceaa941ae6886301ba1375ddf7fa6fdc507665c
ukki <iu.cashirskaia@yandex.ru>
2024-11-14 14:24:55 +0300

    готовая верстка шапки

--- resources/js/Pages/test.vue
+++ resources/js/Pages/test.vue
@@ -2,31 +2,38 @@
 import sliders from '@/Components/smallThings/sliders.vue'
 import swi from '@/Components/smallThings/switch.vue'
 import lightButton from '@/Components/smallThings/buttons/lightButton.vue';
-import InputError from '@/Components/maybeDelete/InputError.vue';
 import avatarIcon from '@/Components/smallThings/avatarIcon.vue';
 </script>
 <template>
  
 <!-- TODO сделать тут добавление этих элементов по затычке иммитирующей бд -->
 
-<div class="w-full h-14 mb-8">
-headplace
+<div class="h-14 mb-8 mx-10 relative">
+    <div class="text-4xl my-4 h-14 inline absolute left-0">EveryDaynik!</div>
+    <div class="h-14 inline absolute right-0" >sjflsdjfkl</div>
 </div>
 
-<div class="mx-10 h-dvh">
+
+
+<div class="mx-10">
     <!-- TODO вынести шапку в отдельный блок -->
     <div class="container flex align-items-center pb-5">
         <avatarIcon ></avatarIcon>
         <h1 class="pl-4 text-2xl ">Settings</h1>
     </div>
-<hr>
-<h1 class="text-lg">Life spheres</h1>
+<hr class="pb-4">
+<div class="pb-10">
+<h1 class="text-lg pb-4">Life spheres</h1>
+<swi class="pb-4"></swi>
 </div>
-
-<!-- <swi></swi>
+<hr class="pb-4">
+<div class="pb-10">
+    <h1 class="text-lg">Spheres weight</h1>
     <sliders></sliders>
-    <div class="flex pl-5">
+</div>
+<div class="flex">
     <lightButton content="сохранить"></lightButton>
     <lightButton content="сбросить"></lightButton>
-</div> -->
+</div>
+</div>
 </template>
